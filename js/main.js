b7c805524baec60c13876352334ed24c7f68202a
Albion <a.selimaj2006@gmail.com>
2024-11-25 14:21:45 -0500

    traduction

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ function render() {
     //this moves the value of the board item into the squares[idx]
     squares[index].textContent = mark;
     });
-    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `C'est une égalité !` : win ? `${win} gagne la partie !` : `C'est au tour de ${turn} !`;
     };
 
 init();
