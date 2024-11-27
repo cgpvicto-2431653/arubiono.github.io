f151d676d1e192855e8bfe274608c04fd429de93
Albion <a.selimaj2006@gmail.com>
2024-11-27 14:13:13 -0500

    fenetre de dialog

--- js/main.js
+++ js/main.js
@@ -64,3 +64,44 @@ function render() {
     };
 
 init();
+
+
+// ma partie 
+//
+//
+//
+//
+//
+//
+
+document.addEventListener('DOMContentLoaded', dialog()); // pour que la function dialog se fasse au chargement de la page 
+    
+function dialog () {
+    const dialog = document.getElementById('dialog');                               // une constant 
+    const closeButton = document.getElementById('fermer-dialog');                   // -----
+    const neverShowButton = document.getElementById('ne-plus-afficher');            // -----
+    const storageKey = 'neverShowDialog';                                           // Bouton pour fermer la fenêtre et enregistrer une préférence pour qu'elle ne s’affiche plus.
+
+
+    // verifier si la fenetre doit être affichée
+    if (!localStorage.getItem(storageKey)) { // si dans local sotage ya pas la clef "neverShowDialog" ca afficher la fenetre en mode modal pour rien pouvoir faire deriere
+        dialog.showModal(); 
+    }
+
+// fermer la fenetre sans modifier le stockage 
+closeButton.addEventListener('click', 
+function () 
+{
+    dialog.close();
+});
+
+// fermer la fenetre et enregistrer dans clef de storage pour qu a la ligne 87 et 88 le dialog de s'affiche jamais 
+// (pour modifer ca faut aller dans le console du navigateur et ecrire "localStorage.removeItem('neverShowDialog');")
+neverShowButton.addEventListener('click', 
+function () 
+{
+    localStorage.setItem(storageKey, 'true');
+    dialog.close();
+});
+};
+
